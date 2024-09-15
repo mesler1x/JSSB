8e753815e36b86ba60c44b9da88778aad69d360c
Roman Mesler <pay2mesler@gmail.com>
2024-09-15 17:07:21 +0500

    feat: add hw 1

---
+++ base/mod1/src/main/java/ru/yandex/femidabackend/Task1.java
@@ -0,0 +1,13 @@
+package ru.yandex.femidabackend;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 1000; i++) {
+            sum += i % 3 == 0 || i % 5 == 0 ? i : 0;
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ base/mod1/src/main/java/ru/yandex/femidabackend/Task2.java
@@ -0,0 +1,15 @@
+package ru.yandex.femidabackend;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[][] x = { {20, 34, 2}, {9, 12, 18}, {3, 4, 5} };
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                min = Math.min(x[i][j], min);
+            }
+        }
+
+        System.out.println(min);
+    }
+}
