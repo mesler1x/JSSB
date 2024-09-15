c94eb271869f1df7bd817ebef7cd9ea4eb8d2ba1
Roman Mesler <pay2mesler@gmail.com>
2024-09-15 17:08:59 +0500

    refactor: rename base package

--- base/mod1/src/main/java/edu/jssb/Task1.java
+++ base/mod1/src/main/java/edu/jssb/Task1.java
@@ -1,4 +1,4 @@
-package ru.yandex.femidabackend;
+package edu.jssb;
 
 
 public class Task1 {
--- base/mod1/src/main/java/edu/jssb/Task2.java
+++ base/mod1/src/main/java/edu/jssb/Task2.java
@@ -1,4 +1,4 @@
-package ru.yandex.femidabackend;
+package edu.jssb;
 
 public class Task2 {
     public static void main(String[] args) {
