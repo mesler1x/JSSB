d72180be342302a3015165884622a10d2dee71a3
Roman Mesler <meslerdev@gmail.com>
2024-12-07 18:43:45 +0500

    MOD-7: finish hw

--- mod7_skillbox/src/main/java/org/example/mod7_skillbox/domain/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package org.example.mod7_skillbox.domain;
-
-public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    DONE
-}
--- mod7_skillbox/src/main/java/org/example/mod7_skillbox/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.mod7_skillbox.repository;
-
-import org.example.mod7_skillbox.domain.Task;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends ReactiveMongoRepository<Task, String> {
-}
